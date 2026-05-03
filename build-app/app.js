55e6554ded08bc6ccf316bd3074eb8459e0cff68
floiteyg <fernando.loiteyg@gmail.com>
2026-05-03 19:09:11 -0300

    Se modifica app.js resolviendo el problema de sintaxis

--- build-app/app.js
+++ build-app/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Simple app for the Building Applications Lab!\n');
 });
 
-app.listen(8080
+app.listen(8080, function () {
   console.log('Simple app for the Building Applications Lab!');
 });
 
